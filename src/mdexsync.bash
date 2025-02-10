2ef0ad4d215bf62e9cfd41993f296188b5b19b52
Steven Benner <admin@stevenbenner.com>
2025-02-10 11:32:49 -0800

    Bump copyright year to 2025

--- src/mdexsync.bash
+++ src/mdexsync.bash
@@ -3,7 +3,7 @@
 # A script to download the specified work from MangaDex and keep the
 # content sychronized with the latest versions on subsequent runs.
 #
-# Copyright 2024 Steven Benner
+# Copyright 2025 Steven Benner
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
